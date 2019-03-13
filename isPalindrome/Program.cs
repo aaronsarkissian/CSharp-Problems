e33eb87193bb2e6ceef99fc6560c26c1bb258cb8
Aaron Sarkissian <aaronsarkissian@gmail.com>
2019-03-13 18:48:35 +0400

    isPalindrome

---
+++ isPalindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace isPalindrome
+{
+    class Program
+    {
+        public static bool IsPalidrom(string str)
+        {
+            str = str.ToLower();
+            var strLen = str.Length;
+            for (var i = 0; i < strLen; i++)
+            {
+                if (str[i] != str[strLen - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(IsPalidrom("Deleveled"));
+        }
+    }
+}
